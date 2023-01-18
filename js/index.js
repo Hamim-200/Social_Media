dcc6f058ec609586109bfeb769d8486bb2c2c703
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-01-18 09:45:44 +0600

    theme customied js added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 // SIDEBAR
-
 const menuItems = document.querySelectorAll('.menu-item');
+//MESSAGE
+const messagesNotification = document.querySelector('#message-notifications');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+//Theme
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customized-theme ');
+
+
+// ========SIDEBAR========
 
 // remove active class from all menu items
 const changeActiveItem = () => {
@@ -26,3 +36,50 @@ menuItems.forEach(item => {
         }
     })
 })
+
+
+// ========MESSAGE========
+
+// SEARCHES CHAT
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
+            user.style.display = 'flex';
+        }
+        else {
+            user.style.display = 'none';
+        }
+    })
+}
+
+// SEARCH CHAT
+messageSearch.addEventListener('keyup', searchMessage);
+
+// MESSAGE BOX
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
+
+// =======Theme Customization=======
+
+//open modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+//close modal
+const closeThememodal = (e) => {
+    if (e.target.classList.contains('customized-theme')) {
+        themeModal.style.display = 'none';
+    }
+}
+
+themeModal.addEventListener('click', closeThememodal);
+theme.addEventListener('click', openThemeModal);
