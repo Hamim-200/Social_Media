f2143ea12c5674dad8c09906cb8585d7d57bd5ef
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-01-18 11:57:55 +0600

    primary class change

--- js/index.js
+++ js/index.js
@@ -8,8 +8,9 @@ const messageSearch = document.querySelector('#message-search');
 //Theme
 const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customized-theme ');
-
-
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
 // ========SIDEBAR========
 
 // remove active class from all menu items
@@ -83,3 +84,80 @@ const closeThememodal = (e) => {
 
 themeModal.addEventListener('click', closeThememodal);
 theme.addEventListener('click', openThemeModal);
+
+// FONSTS
+// Remove active class from spans or font size selector
+
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+fontSizes.forEach(size => {
+    size.addEventListener('click', () => {
+        removeSizeSelector()
+        let fontSize;
+        size.classList.toggle('active');
+
+        if (size.classList.contains('font-size-1')) {
+            fontSize = '10px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '5.4rem');
+        }
+        else if (size.classList.contains('font-size-2')) {
+            fontSize = '13px';
+            root.style.setProperty('----sticky-top-left', '5.4rem');
+            root.style.setProperty('----sticky-top-right', '-7rem');
+        }
+        else if (size.classList.contains('font-size-3')) {
+            fontSize = '16px';
+            root.style.setProperty('----sticky-top-left', '-2rem');
+            root.style.setProperty('----sticky-top-right', '-17rem');
+        }
+        else if (size.classList.contains('font-size-4')) {
+            fontSize = '19px';
+            root.style.setProperty('----sticky-top-left', '-5rem');
+            root.style.setProperty('----sticky-top-right', '-25rem');
+        }
+        else if (size.classList.contains('font-size-5')) {
+            fontSize = '22px';
+            root.style.setProperty('----sticky-top-left', '-12rem');
+            root.style.setProperty('----sticky-top-right', '-35rem');
+        }
+        //change font size of the root html element
+        document.querySelector('html').style.fontSize = fontSize;
+    })
+
+})
+// REMOVE ACTIVE CLASS
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active')
+    })
+}
+// CHANGE PRIMARY COLOR
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        let primaryHue;
+        changeActiveColorClass();
+
+        if (color.classList.contains('color-1')) {
+            primaryHue = 252;
+        }
+        else if (color.classList.contains('color-2')) {
+            primaryHue = 52;
+        }
+        else if (color.classList.contains('color-3')) {
+            primaryHue = 352;
+        }
+        else if (color.classList.contains('color-4')) {
+            primaryHue = 152;
+        }
+        else if (color.classList.contains('color-5')) {
+            primaryHue = 202;
+        }
+        color.classList.add('active')
+        root.style.setProperty('--primary-color-hue', primaryHue)
+    })
+})
